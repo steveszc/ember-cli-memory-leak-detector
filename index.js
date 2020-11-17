0d3d5c7547342a867f4f7affbb14a1898cf17f73
Steve Szczecina <steveszc@gmail.com>
2020-11-16 23:17:42 -0700

    add config - configurable debugging port - configurable error on detection - configurable enabled flag

--- index.js
+++ index.js
@@ -7,8 +7,22 @@ const attachMiddleware = require("./lib/attach-middleware");
 module.exports = {
   name: require("./package").name,
 
-  contentFor(type) {
-    if (type === "test-body-footer" && this._isMemoryLeakDetectionEnabled()) {
+  config() {
+    return {
+      "ember-cli-memory-leak-detector": {
+        enabled: true,
+        error: true,
+        debugPort: 9222,
+      },
+    };
+  },
+
+  contentFor(type, config) {
+    if (
+      type === "test-body-footer" &&
+      config["ember-cli-memory-leak-detector"] &&
+      config["ember-cli-memory-leak-detector"].enabled
+    ) {
       const template = fs
         .readFileSync(
           path.join(__dirname, "lib", "templates", "test-body-footer.html")
@@ -21,18 +35,10 @@ module.exports = {
   },
 
   serverMiddleware({ app }) {
-    if (this._isMemoryLeakDetectionEnabled()) {
-      attachMiddleware({ app, addon: this });
-    }
+    attachMiddleware({ app, addon: this });
   },
 
   testemMiddleware(app) {
-    if (this._isMemoryLeakDetectionEnabled()) {
-      attachMiddleware({ app, addon: this });
-    }
-  },
-
-  _isMemoryLeakDetectionEnabled() {
-    return true;
+    attachMiddleware({ app, addon: this });
   },
 };
