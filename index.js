58c4b157daac079ec07ac8672a4807d8230f3490
Steve Szczecina <steveszc@gmail.com>
2020-11-18 16:02:34 -0700

    cleanup lint rules

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const path = require("path");
-const fs = require("fs-extra");
+const fs = require("fs");
 const attachMiddleware = require("./lib/attach-middleware");
 
 module.exports = {
