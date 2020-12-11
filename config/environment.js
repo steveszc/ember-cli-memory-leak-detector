d6b0ca5a20ce1114c999a07d451993becc138106
Steve Szczecina <steveszc@gmail.com>
2020-12-11 10:04:41 -0700

    improve config logic

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,12 @@
 'use strict';
 
-module.exports = function(/* environment, appConfig */) {
-  return { };
+module.exports = function (/* environment, appConfig */) {
+  return {
+    'ember-cli-memory-leak-detector': {
+      enabled: true,
+      error: true,
+      remoteDebuggingPort: 9222,
+      ignoreClasses: ['App'],
+    },
+  };
 };
--- index.js
+++ index.js
@@ -7,24 +7,8 @@ const attachMiddleware = require("./lib/attach-middleware");
 module.exports = {
   name: require("./package").name,
 
-  config() {
-    return {
-      "ember-cli-memory-leak-detector": {
-        enabled: true,
-        error: true,
-        remoteDebuggingPort: 9222,
-        ignoreClasses: ["App"],
-      },
-    };
-  },
-
-  contentFor(type, config) {
-    if (
-      type === "test-body-footer" &&
-      config["ember-cli-memory-leak-detector"] &&
-      config["ember-cli-memory-leak-detector"].enabled &&
-      this.isEnabled()
-    ) {
+  contentFor(type) {
+    if (type === "test-body-footer" && this.isEnabled()) {
       const template = fs
         .readFileSync(
           path.join(__dirname, "lib", "templates", "test-body-footer.html")
@@ -50,6 +34,7 @@ module.exports = {
 
   isEnabled() {
     const isDirectDependency = this.parent === this.project;
-    return isDirectDependency;
+    const { enabled } = this.project.config()[this.name];
+    return isDirectDependency && enabled;
   },
 };
