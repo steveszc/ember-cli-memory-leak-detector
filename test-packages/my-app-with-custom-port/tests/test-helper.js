01c00d881d1ea9da2c4650078dfde1b86862947e
Steve Szczecina <steveszc@gmail.com>
2021-03-15 10:01:52 -0600

    add tests for app-with-custom-port
    uses monorepo structure to add an ember app
    using a custom remote debugging port for testing

---
+++ test-packages/my-app-with-custom-port/tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'my-app-with-custom-port/app';
+import config from 'my-app-with-custom-port/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
